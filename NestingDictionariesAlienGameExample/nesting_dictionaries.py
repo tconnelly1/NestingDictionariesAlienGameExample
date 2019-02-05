70515b3de0a5bd6567f020f2b4cff7a1b3fc19a6
Tom <Tom@DESKTOP-MCJJ2VL.attlocal.net>
2019-02-05 15:36:07 -0600

    commit

--- NestingDictionariesAlienGameExample/nesting_dictionaries.py
+++ NestingDictionariesAlienGameExample/nesting_dictionaries.py
@@ -17,10 +17,7 @@ for alien in aliens[0:3]:
         alien["color"] = "yellow"
         alien["speed"] = "medium"
         alien["points"] = 10
-    elif alien["color"] == "yellow":
-        alien["color"] = "red"
-        alien["speed"] = "fast"
-        alien["points"] = 15
+
     
 # Show the first 5 aliens
 for alien in aliens[:5]:
